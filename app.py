17b0495c9b9b24617881babb85f0a26c8b07a5da
Ikenna Nwekwo <ikennanwekwo@gmail.com>
2025-06-26 22:59:59 +0100

    Set up dynamic port for Render

--- app.py
+++ app.py
@@ -42,4 +42,9 @@ def comment(index):
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
+
